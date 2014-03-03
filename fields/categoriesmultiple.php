f14019c56cb646ccd59f2addf78215822eb53362
Parvez <parvez@themexpert.com>
2014-03-03 16:52:09 +0600

    k2 compatibility fix

--- fields/categoriesmultiple.php
+++ fields/categoriesmultiple.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @package Xpert Contents
- * @version 1.4
+ * @package Xpert Captions
+ * @version 2.5
  * @author ThemeXpert http://www.themexpert.com
  * @copyright Copyright (C) 2009 - 2011 ThemeXpert
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
@@ -10,144 +10,51 @@
 
 // no direct access
 defined('_JEXEC') or die('Restricted access');
-
-if(K2_JVERSION=='16'){
-	jimport('joomla.form.formfield');
-	class JFormFieldCategoriesMultiple extends JFormField {
-
-		var	$type = 'categoriesmultiple';
-
-		function getInput(){
-			return JElementCategoriesMultiple::fetchElement($this->name, $this->value, $this->element, $this->options['control']);
-		}
-	}
-}
-
-jimport('joomla.html.parameter.element');
-
-class JElementCategoriesmultiple extends JElement
+if( file_exists( JPATH_SITE .'/components/com_k2/k2.php') )
 {
 
-	var	$_name = 'categoriesmultiple';
+    jimport('joomla.form.formfield');
 
-	function fetchElement($name, $value, &$node, $control_name){
-		$params = &JComponentHelper::getParams('com_k2');
-		
-		$document = &JFactory::getDocument();
-		
-		if(version_compare(JVERSION,'1.6.0','ge')) {
-			JHtml::_('behavior.framework');
-		} else {
-			JHTML::_('behavior.mootools');
-		}
+    class JFormFieldCategoriesMultiple extends JFormField
+    {
 
-		$db = &JFactory::getDBO();
-		$query = 'SELECT m.* FROM #__k2_categories m WHERE published=1 AND trash = 0 ORDER BY parent, ordering';
-		$db->setQuery( $query );
-		$mitems = $db->loadObjectList();
-		$children = array();
-		if ($mitems){
-			foreach ( $mitems as $v ){
-				if(K2_JVERSION=='16'){
-					$v->title = $v->name;
-					$v->parent_id = $v->parent;
-				}
-				$pt = $v->parent;
-				$list = @$children[$pt] ? $children[$pt] : array();
-				array_push( $list, $v );
-				$children[$pt] = $list;
-			}
-		}
-		$list = JHTML::_('menu.treerecurse', 0, '', array(), $children, 9999, 0, 0 );
-		$mitems = array();
+        var	$type = 'categoriesmultiple';
 
-		foreach ( $list as $item ) {
-			$item->treename = JString::str_ireplace('&#160;', '- ', $item->treename);
-			$mitems[] = JHTML::_('select.option',  $item->id, '   '.$item->treename );
-		}
+        function getInput(){
+            $params = JComponentHelper::getParams('com_k2');
 
-		$doc = & JFactory::getDocument();
-		if(K2_JVERSION=='16'){
-			$js = "
-			var \$K2 = jQuery.noConflict();
-			\$K2(document).ready(function(){
-				
-				\$K2('#jform_params_catfilter0').click(function(){
-					\$K2('#jformparamscategory_id').attr('disabled', 'disabled');
-					\$K2('#jformparamscategory_id option').each(function() {
-						\$K2(this).attr('selected', 'selected');
-					});
-				})
-				
-				\$K2('#jform_params_catfilter1').click(function(){
-					\$K2('#jformparamscategory_id').removeAttr('disabled');
-					\$K2('#jformparamscategory_id option').each(function() {
-						\$K2(this).removeAttr('selected');
-					});
-	
-				})
-				
-				if (\$K2('#jform_params_catfilter0').attr('checked')) {
-					\$K2('#jformparamscategory_id').attr('disabled', 'disabled');
-					\$K2('#jformparamscategory_id option').each(function() {
-						\$K2(this).attr('selected', 'selected');
-					});
-				}
-				
-				if (\$K2('#jform_params_catfilter1').attr('checked')) {
-					\$K2('#jformparamscategory_id').removeAttr('disabled');
-				}
-				
-			});
-			";			
-				
-		}
-		else {
-			$js = "
-			var \$K2 = jQuery.noConflict();
-			\$K2(document).ready(function(){
-				
-				\$K2('#paramscatfilter0').click(function(){
-					\$K2('#paramscategory_id').attr('disabled', 'disabled');
-					\$K2('#paramscategory_id option').each(function() {
-						\$K2(this).attr('selected', 'selected');
-					});
-				})
-				
-				\$K2('#paramscatfilter1').click(function(){
-					\$K2('#paramscategory_id').removeAttr('disabled');
-					\$K2('#paramscategory_id option').each(function() {
-						\$K2(this).removeAttr('selected');
-					});
-	
-				})
-				
-				if (\$K2('#paramscatfilter0').attr('checked')) {
-					\$K2('#paramscategory_id').attr('disabled', 'disabled');
-					\$K2('#paramscategory_id option').each(function() {
-						\$K2(this).attr('selected', 'selected');
-					});
-				}
-				
-				if (\$K2('#paramscatfilter1').attr('checked')) {
-					\$K2('#paramscategory_id').removeAttr('disabled');
-				}
-				
-			});
-			";			
-				
-				
-		}
+            $db = JFactory::getDBO();
+            $query = 'SELECT m.* FROM #__k2_categories m WHERE published=1 AND trash = 0 ORDER BY parent, ordering';
+            $db->setQuery( $query );
+            $mitems = $db->loadObjectList();
+            $children = array();
+            if ($mitems){
+                foreach ( $mitems as $v ){
+                    $v->title = $v->name;
+                    $v->parent_id = $v->parent;
+                    $pt = $v->parent;
+                    $list = @$children[$pt] ? $children[$pt] : array();
+                    array_push( $list, $v );
+                    $children[$pt] = $list;
+                }
+            }
+            $attr  = 'class="inputbox chzn-select"';
+            $attr .= 'style="width:280px;"';
+            $attr .= 'multiple="multiple"';
+            $attr .= 'data-placeholder="Click here to select categories"';
 
-		if(K2_JVERSION=='16'){
-			$fieldName = $name.'[]';
-		}
-		else {
-			$fieldName = $control_name.'['.$name.'][]';
-		}
+            $list = JHTML::_('menu.treerecurse', 0, '', array(), $children, 9999, 0, 0 );
+            $mitems = array();
 
-		$doc->addScriptDeclaration($js);
-		$output= JHTML::_('select.genericlist',  $mitems, $fieldName, 'class="inputbox" style="width:90%;" multiple="multiple" size="10"', 'value', 'text', $value );
-		return $output;
-	}
+            foreach ( $list as $item ) {
+                $item->treename = JString::str_ireplace('&#160;', '- ', $item->treename);
+                $mitems[] = JHTML::_('select.option',  $item->id, '   '.$item->treename );
+            }
+
+            $fieldName = $this->name.'[]';
+
+            $output= JHTML::_('select.genericlist',  $mitems, $fieldName, trim($attr), 'value', 'text', $this->value );
+            return $output;
+        }
+    }
 }
